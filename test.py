f47f0145a564bba9be32f90e7c8cd5ff7d419d06
Thomas Mathis <totolego88@gmail.com>
2021-03-19 10:47:13 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,24 +5,25 @@ pygame.init()
 
 continuer = True
 
-screen = pygame.display.set_mode((1280, 780))
+screen = pygame.display.set_mode((1280, 700))
 fond = pygame.image.load("background.png").convert()
 screen.blit(fond, (0,0))
+
+new = pygame.image.load("option.png")
+new = pygame.transform.scale(new, (250, 100))
+new_rect = new.get_rect()
+new_rect.x, new_rect.y = 200, 100
 
 play = pygame.image.load("play.jpg")
 play = pygame.transform.scale(play, (250, 100))
 play_rect = play.get_rect()
-play_rect.x, play_rect.y = 100, 400
+play_rect.x, play_rect.y = 200, 300
 
 Quit = pygame.image.load("quit.jpg")
 Quit = pygame.transform.scale(Quit, (250, 100))
 Quit_rect = Quit.get_rect()
-Quit_rect.x, Quit_rect.y = 100, 550
+Quit_rect.x, Quit_rect.y = 200, 450
 
-option = pygame.image.load("option.png")
-option = pygame.transform.scale(option, (50, 50))
-option_rect = option.get_rect()
-option_rect.x, option_rect.y = 1180, 50
 
 while continuer:
     
@@ -31,7 +32,7 @@ while continuer:
         
     screen.blit(Quit, Quit_rect)
 
-    screen.blit(option, option_rect)
+    screen.blit(new, new_rect)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -42,6 +43,6 @@ while continuer:
             print("coucou c'est moi")
         if Quit_rect.collidepoint(x,y) and event.type == pygame.MOUSEBUTTONUP:
             pygame.quit()
-        if option_rect.collidepoint(x,y) and event.type == pygame.MOUSEBUTTONUP:
+        if new_rect.collidepoint(x,y) and event.type == pygame.MOUSEBUTTONUP:
             print("coucou c'est elle")
     pygame.display.update()
