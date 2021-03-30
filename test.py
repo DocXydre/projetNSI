bc46269fa393360035b7574bb2bfa3ea3914fc4f
Thomas Mathis <totolego88@gmail.com>
2021-03-30 10:44:44 +0200

    Update test.py

--- test.py
+++ test.py
@@ -24,16 +24,12 @@ Quit = pygame.transform.scale(Quit, (250, 100))
 Quit_rect = Quit.get_rect()
 Quit_rect.x, Quit_rect.y = 200, 450
 
+Menu=False
 
 while continuer:
-    
-    
-    screen.blit(play, play_rect)
-        
     screen.blit(Quit, Quit_rect)
-
+    screen.blit(play, play_rect)
     screen.blit(new, new_rect)
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             continuer = False
@@ -42,7 +38,24 @@ while continuer:
         if play_rect.collidepoint(x,y) and event.type == pygame.MOUSEBUTTONUP:
             print("coucou c'est moi")
         if Quit_rect.collidepoint(x,y) and event.type == pygame.MOUSEBUTTONUP:
-            pygame.quit()
+            continuer = 0
+            Menu=0
         if new_rect.collidepoint(x,y) and event.type == pygame.MOUSEBUTTONUP:
-            print("coucou c'est elle")
+            continuer = False
+            Menu = True
     pygame.display.update()
+
+while Menu:
+    screen.blit(fond, (0,0))
+    screen.blit(Quit, Quit_rect)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Menu = False
+            pygame.quit()
+        x,y = pygame.mouse.get_pos()
+        if Quit_rect.collidepoint(x,y) and event.type == pygame.MOUSEBUTTONUP:
+            Menu=0
+            continuer = 0
+    
+    pygame.display.update()
+pygame.quit()
